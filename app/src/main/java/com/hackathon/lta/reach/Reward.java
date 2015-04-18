bb557b4ee6a252e83f55c6024e9a57b03e481eb2
StormFalcon <flyerwaitsun@gmail.com>
2015-04-19 06:34:25 +0800

    - Added placeholder for social and reward

--- app/src/main/java/com/hackathon/lta/reach/Reward.java
+++ app/src/main/java/com/hackathon/lta/reach/Reward.java
@@ -1,9 +1,12 @@
 package com.hackathon.lta.reach;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 
 public class Reward extends ActionBarActivity {
@@ -12,8 +15,23 @@ public class Reward extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reward);
+
+        getSupportActionBar().hide();
+
+        final ImageButton backButton = (ImageButton) findViewById(R.id.back);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                back();
+            }
+        });
     }
 
+    public void back()
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
